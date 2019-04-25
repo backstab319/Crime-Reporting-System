aa3638464e08444ac9ee68fbd68532ce94ece153
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-04-26 00:55:55 +0530

    Create person_data.php

---
+++ data/person_data.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="utf-8">
+    <title>Missing Person</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+
+    <?php
+        if(!isset($_COOKIE["user"])){
+        echo "<div class='container jumbotron text-center mt-2'>
+        <p class='lead'>Please login back in again to continue browsing CRS!</p>
+        </div>";
+        exit();
+        }
+    ?>
+
+    <div class="navbar-section home">
+        <navbar class="navbar navbar-expand-sm navbar-light bg-light fixed-top">
+            <div class="container">
+                <a class="navbar-brand">CRS</a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#toggle"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="toggle">
+                    <div class="navbar-menu ml-auto">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/index.php">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="missing_person.php">Add/Update Person Data</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </navbar>
+    </div>
+
+    <div class="container text-center col-lg-7 col-xl-7">
+        
+        <h1 class="display-4">Person Data</h1>
+        
+            <?php
+                include "../connect.php";
+                show_person();
+                function show_person(){
+                    global $conn;
+                    $sql = "SELECT * FROM person_data";
+                    $result = $conn->query($sql);
+                    if($result->num_rows > 0){
+                        show($result);
+                    }else{
+                        echo "<p class='lead text-center'>There are no person's data</p>";
+                    }
+                }
+                function show($result){
+                    echo '<table class="table table-bordered table-striped table-hover">
+                    <thead class="thead-primary">
+                    <tr>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Gender</th>
+                    <th>Last Seen</th>
+                    <th>Height</th>
+                    <th>Delete</th>
+                    </tr>
+                    </thead>';
+                    while($row = $result->fetch_assoc()){
+                        echo "<tr><td><img src='".$row['image']."' class='img-thumbnail'></td><td>".$row['name']."</td><td>".$row['gender']."</td><td>".$row['last_seen']."</td><td>".$row['height']."</td><td><a class='btn btn-primary' href='person_data.php?name=".$row['name']."'>Delete</a></td></tr>";
+                    }
+                    echo "</table>";
+                }
+                if(isset($_GET["name"])){
+                    delete($_GET["name"]);
+                }
+                function delete($user){
+                    global $conn;
+                    $sql = "DELETE FROM person_data WHERE name='$user'";
+                    $conn->query($sql);
+                    echo "<script type='text/javascript'>document.location = 'person_data.php';</script>";
+                }
+            ?>
+    </div>
+    
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
