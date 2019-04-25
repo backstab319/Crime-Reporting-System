0002d4ecfe269514f9fa6314a66006767f75d8b7
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-04-25 22:06:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+    function userinit($username){
+        setcookie("user",$username,time()+1800,"/");
+    }
+?>
 <!DOCTYPE html>
 <html>
 <head lang="en">
@@ -19,6 +24,9 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="/index.php">Home</a>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/signup.php">Sign up</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -27,26 +35,71 @@
     </div>
 
     <div class="background">
-        <div class="y-axis">
-            <div class="d-flex flex-row container">
-                <div class="d-flex justify-content-start container-fluid">
-                    <div class="col-lg-8 col-xl-8 text-center text-justify">
-                        <div class="login-data">
-                            <h1 class="display-4">Welcome to Crime Reporting System</h1>
-                        </div>
+        <div class="overlay">
+            <div class="d-flex flex-row container pt-4">
+                <div class="mr-auto">
+                    <div class="content">
+                        <h1 class="display-4 text-center text-primary text-justify">Crime Reporting System</h1>
+                        <p class="lead text-light text-center text-justify">Welcome to crime reporting system. Please sign up to report any crime in your area or report any missing person 
+                        or any theft that has occured.
+                        </p>
                     </div>
                 </div>
-                <div class="d-flex justify-content-end container-fluid">
-                    <div class="col-lg-4 col-xl-4 text-center text-justify">
-                        <div class="form-group">
-                            <h1 class="display-4 mt-4">Login</h1>
+                <div class="col-lg-4 col-xl-4">
+                    <div class="form-group">
+                        <h1 class="display-4 text-center text-primary">Login</h1>
+                        <form action="index.php" method="POST">
                             <input type="text" name="username" placeholder="Username" class="form-control mb-2">
                             <input type="password" name="password" placeholder="Password" class="form-control mb-2">
-                            <input type="submit" name="submit" value="Login" class="form-control btn btn-primary mb-2">
-                        </div>
+                            <input type="submit" name="submit" value="Login" class="form-control btn btn-outline-primary mb-2">
+                        </form>
                     </div>
+                    <?php
+                        include "connect.php";
+                        if(isset($_POST["submit"])){
+                            login();
+                        }
+                        function login(){
+                            global $conn;
+                            $user = $_POST["username"];
+                            $pass = $_POST["password"];
+                            $sql = "SELECT * FROM login_details WHERE user_id='$user'";
+                            $result = $conn->query($sql);
+                            if($result->num_rows > 0){
+                                redirect($result,$user);
+                            }else{
+                                echo "<p class='lead text-danger'>Wrong password!</p>";
+                            }
+                        }
+                        function redirect($result,$user){
+                            userinit($user);
+                            $row = $result->fetch_assoc();
+                            if($row["user_type"] == 'admin'){
+                                header("Location: admin.php");
+                            }elseif($row["user_type"] == 'station'){
+                                header("Location: station.php");
+                            }else{
+                                header("Location: user.php");
+                            }
+                        }
+                    ?>
                 </div>
             </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="container text-center text-justify">
+            <h1 class="display-4 text-primary">Latest News</h1>
+        </div>
+        <div class="container text-center text-justify">
+            <h1 class="display-4 text-primary">Missing Individuals</h1>
+        </div>
+        <div class="container text-center text-justify">
+            <h1 class="display-4 text-primary">Missing Vehicles</h1>
+        </div>
+        <div class="container text-center text-justify">
+            <h1 class="display-4 text-primary">Dangerous Criminals</h1>
         </div>
     </div>
     
