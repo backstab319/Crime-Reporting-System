b619d3bd5b90a864dcf36a0d917d8d64686a6382
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-04-26 00:32:48 +0530

    Create updatec_data.php

---
+++ data/updatec_data.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="utf-8">
+    <title>Update Criminal Data</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+
+    <?php
+        if(!isset($_COOKIE["user"])){
+        echo "<div class='container jumbotron text-center mt-2'>
+        <p class='lead'>Please login back in again to continue browsing CRS!</p>
+        </div>";
+        exit();
+        }
+    ?>
+
+    <div class="navbar-section home">
+        <navbar class="navbar navbar-expand-sm navbar-light bg-light fixed-top">
+            <div class="container">
+                <a class="navbar-brand">CRS</a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#toggle"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="toggle">
+                    <div class="navbar-menu ml-auto">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/index.php">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="criminal_data.php">View Criminal</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </navbar>
+    </div>
+
+    <div class="text-center center-content container d-flex justify-content-center align-items-center">
+        <div class="form-group col-lg-7 col-xl-7 text-center">
+            <form action="updatec_data.php" method="POST">
+            <h1 class="display-4">Update Criminal's Data</h1>
+            <input type="text" class="form-control my-2" name="oldval" placeholder="Enter old value">
+                    <label for="column">Select Information to change</label>
+                    <select class="form-control my-2" name="column" id="column">
+                        <option value="image">Image</option>
+                        <option value="name">Name</option>
+                        <option value="gender">PGender</option>
+                        <option value="last_seen">Last Seen</option>
+                        <option value="height">Height</option>
+                        <option value="charges">Charges</option>
+                    </select>
+                    <input type="text" class="form-control my-2" name="newval" placeholder="Enter new value">
+                    <input type="submit" class="form-control my-2 btn-outline-primary" value="Update" name="update">
+            </form>
+            <?php
+                include "../connect.php";
+                if(isset($_POST["update"])){
+                    update();
+                }
+                function update(){
+                    global $conn;
+                    $oldval = $_POST["oldval"];
+                    $column = $_POST["column"];
+                    $newval = $_POST["newval"];
+                    if(($oldval and $column and $newval)!= NULL){
+                        $sql = "UPDATE criminal_data SET $column='$newval' WHERE $column='$oldval'";
+                        echo "<p class='lead text-danger'>Data Updated</p>";
+                        $conn->query($sql);
+                    }else{
+                        echo "<p class='lead text-danger'>Please check all input before proceeding</p>";
+                    }
+                }
+            ?>
+        </div>
+    </div>
+    
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
