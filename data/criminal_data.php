ec476959aa2a6a5b748c80af0314b45e1dccdfe8
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-04-26 00:55:48 +0530

    Update criminal_data.php

--- data/criminal_data.php
+++ data/criminal_data.php
@@ -84,8 +84,6 @@
                     echo "<script type='text/javascript'>document.location = 'criminal_data.php';</script>";
                 }
             ?>
-        
-        
     </div>
     
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
