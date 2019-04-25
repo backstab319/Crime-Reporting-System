5228a84d4419f264a9a54c796439e1bea517e5eb
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-04-26 04:27:10 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="utf-8">
+    <title>About</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+
+    <div class="navbar-section home">
+        <navbar class="navbar navbar-expand-sm navbar-light bg-light fixed-top">
+            <div class="container">
+                <a class="navbar-brand">CRS</a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#toggle"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="toggle">
+                    <div class="navbar-menu ml-auto">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/index.php">Home</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </navbar>
+    </div>
+
+    <div class="jumbotron container text-center text-center col-lg-6 col-xl-6 my-3">
+        <h1 class="display-4">About Crime Reporting System</h1>
+        <p class="lead text-center text-justify">
+        Online Crime Reporting System is a web based application that is developed using PHP and MySQL. 
+        The Objective of Crime Reporting System is to develop a web based application using which people can report crime online. 
+        It provides the facility of uploading images or videos of crime scenes so that police can take action 
+        immediately. It also provides the information of missing persons, most wanted criminals and safety tips for the awareness of people.
+        </p>
+    </div>
+
+    <div class="container text-center col-lg-6 col-xl-6 my-3">
+        <h1 class="display-4">Languages used</h1>
+        <ul class="list-group">
+            <li class="list-group-item">HTML</li>
+            <li class="list-group-item">CSS</li>
+            <li class="list-group-item">Javascript</li>
+            <li class="list-group-item">Bootstrap 4</li>
+            <li class="list-group-item">PHP</li>
+            <li class="list-group-item">My Sql</li>
+        </ul>
+    </div>
+
+    <div class="jumbotron container text-center text-center col-lg-6 col-xl-6 my-3">
+        <h1 class="display-4">About Developer</h1>
+        <p class="lead text-center text-justify">
+            This Project is actively developed by Santhos G of VI Semester BCA 'B' Section
+        </p>
+    </div>
+    
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
