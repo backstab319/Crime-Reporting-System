55bbabbaa4808b0acbe9c2caf02d75e1f5c92668
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-04-26 04:27:04 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -29,12 +29,38 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="/index.php">Home</a>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/data/news.php">News</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
             </div>
         </navbar>
     </div>
+
+    <div class="admin d-flex flex-column">
+        <div class="d-flex align-items-start justify-content-center">
+            <div class="data container text-center">
+            <h1 class="display-4">Manage Users</h1>
+            <a href="/manage/manage_users.php" class="btn btn-primary">Manage Users</a>
+            </div>
+        </div>
+        <div class="d-flex align-items-start justify-content-center mt-auto">
+            <div class="data container text-center">
+            <h1 class="display-4">Manage Stations</h1>
+            <a href="/manage/manage_stations.php" class="btn btn-primary">Manage Stations</a>
+            </div>
+        </div>
+        <div class="d-flex align-items-end justify-content-center mt-auto">
+            <div class="data container text-center">
+            <h1 class="display-4">Saftey Tips</h1>
+            <a href="/manage/tips.php" class="btn btn-primary">Saftey Tips</a>
+            </div>
+        </div>
+    </div>
+
+    
     
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
